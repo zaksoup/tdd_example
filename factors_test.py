80b671fd2838175b3dbfec30763bd649368801e0
Zachary Auerbach <zak@zaksoup.com>
2014-09-16 17:03:31 -0700

    Change test to reflect new understanding of return values

--- factors_test.py
+++ factors_test.py
@@ -4,8 +4,8 @@ import unittest
 class TestRecursionMuscles(unittest.TestCase):
 
   def test_prime_factors(self):
-    self.assertEqual(factors(2), 2)
-    self.assertEqual(factors(3), 3)
+    self.assertEqual(factors(2), [2])
+    self.assertEqual(factors(3), [3])
 
 if __name__ == '__main__':
     unittest.main()
