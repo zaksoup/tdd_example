5966261af5db33d21c5eebca4a08ef0e5af203ce
Zachary Auerbach <zak@zaksoup.com>
2014-09-17 00:26:50 -0700

    making it really hard on ourselves now

--- factors_test.py
+++ factors_test.py
@@ -7,6 +7,7 @@ class TestRecursionMuscles(unittest.TestCase):
     self.assertEqual(factors(2), [2])
     self.assertEqual(factors(3), [3])
     self.assertEqual(factors(4), [2, 2])
+    self.assertEqual(factors(51), [3, 17])
 
 if __name__ == '__main__':
     unittest.main()
