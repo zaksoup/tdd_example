1c1ebdfd48f9306dfe7d1c47020b757eb8ae0ab5
Zachary Auerbach <zak@zaksoup.com>
2014-09-16 17:01:19 -0700

    New assertion, now failing

--- factors_test.py
+++ factors_test.py
@@ -5,6 +5,7 @@ class TestRecursionMuscles(unittest.TestCase):
 
   def test_prime_factors(self):
     self.assertEqual(factors(2), 2)
+    self.assertEqual(factors(3), 3)
 
 if __name__ == '__main__':
     unittest.main()
